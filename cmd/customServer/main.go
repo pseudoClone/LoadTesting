2465b46883114731da8b9267b6947fed56fdca7a
pseudoclone <prince.077bei027@acem.edu.np>
2026-07-03 13:59:47 +0545

    Remove terminal logging bottleneck

--- cmd/customServer/main.go
+++ cmd/customServer/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Downloadable body string")
-		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+		// log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
 	})
 	fmt.Println("Server running on :8000")
 	http.ListenAndServe(":8000", nil)
