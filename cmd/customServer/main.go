dda2a5f20dc1fbee0a29215d79af46c9d11978f9
pseudoclone <prince.077bei027@acem.edu.np>
2026-07-03 09:31:33 +0545

    Create custom server instead of Python development server

--- cmd/customServer/main.go
+++ cmd/customServer/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Downloadable body string")
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+	})
+	fmt.Println("Server running on :8000")
+	http.ListenAndServe(":8000", nil)
+}
